02b670be0ef84613b86eab8583ad2230cf2ec6f3
Macias92 <maciejstys92@gmail.com>
2023-01-17 20:09:22 +0100

    created CustomUserManager

--- authx/models.py
+++ authx/models.py
@@ -1,8 +1,70 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import PermissionMixin, AbstractBaseUser
+from django.contrib.auth.models import PermissionMixin, AbstractBaseUser, BaseUserManager
 
 from .common import ROLES
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_cashier(self, username, email, password):
+        user = self.create_user(
+            username,
+            email,
+            password=password,
+        )
+
+        user.role = 1
+        user.save(using=self._db)
+        return user
+
+    def create_barista(self, username, email, password):
+        user = self.create_user(
+            username,
+            email,
+            password=password,
+        )
+
+        user.role = 2
+        user.save(using=self._db)
+        return user
+
+    def create_manager(self, username, email, password):
+        user = self.create_user(
+            username,
+            email,
+            password=password,
+        )
+
+        user.role = 3
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        user = self.create_user(
+            username,
+            email,
+            password=password,
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.role = 4
+        user.save(using=self._db)
+        return user
 
 
 class CustomUser(AbstractBaseUser, PermissionMixin):
